c9bdb09189739af965a7c9a16ade551236221eb9
dallepep <piero.dallepezze@gmail.com>
2015-04-16 15:17:09 +0100

    Replaced junit SNPFrequenciesTest with the generic VariantCallDetectionTest

--- test/java/uk/ac/babraham/BamQC/Modules/VariantCallDetectionTest.java
+++ test/java/uk/ac/babraham/BamQC/Modules/VariantCallDetectionTest.java
@@ -41,15 +41,15 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import uk.ac.babraham.BamQC.Modules.SNPFrequencies;
+import uk.ac.babraham.BamQC.Modules.VariantCallDetection;
 import uk.ac.babraham.BamQC.Sequence.SequenceFormatException;
 
-public class SNPFrequenciesTest {
+public class VariantCallDetectionTest {
 
-	private static Logger log = Logger.getLogger(SNPFrequenciesTest.class);
+	private static Logger log = Logger.getLogger(VariantCallDetectionTest.class);
 	
 	private List<SAMRecord> samRecords = null;
-	private SNPFrequencies snpFrequencies = null;
+	private VariantCallDetection variantCallDetection = null;
 	
 	// Load the whole SAM file, as this is very short. (3-10 lines).
 	// Clearly this is not a correct approach generally.
@@ -95,25 +95,25 @@ public class SNPFrequenciesTest {
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		System.out.println("Set up : SNPFrequencies");
+		System.out.println("Set up : VariantCallDetection");
 		SAMFileReader.setDefaultValidationStringency(SAMFileReader.ValidationStringency.SILENT);		
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		System.out.println("Tear down : SNPFrequencies");
+		System.out.println("Tear down : VariantCallDetection");
 	}		
 	
 	@Before
 	public void setUp() throws Exception {
 		samRecords = new ArrayList<SAMRecord>();		
-		snpFrequencies = new SNPFrequencies();		
+		variantCallDetection = new VariantCallDetection();		
 	}
 
 	@After
 	public void tearDown() throws Exception { 
 		samRecords = null;
-		snpFrequencies = null;		
+		variantCallDetection = null;		
 	}
 
 	
@@ -124,10 +124,10 @@ public class SNPFrequenciesTest {
 		
 		TestObjectFactory testObjectFactory = new TestObjectFactory();
 		samRecords = testObjectFactory.getSamRecords();	
-		snpFrequencies = new SNPFrequencies();
+		variantCallDetection = new VariantCallDetection();
 		
 		for (SAMRecord samRecord : samRecords) {
-			snpFrequencies.processSequence(samRecord);
+			variantCallDetection.processSequence(samRecord);
 		}	
 	}
 	
@@ -138,8 +138,8 @@ public class SNPFrequenciesTest {
 		List<String> combinedCigarMDtagList = new ArrayList<String>();
 		for (SAMRecord samRecord : samRecords) {
 			//printCigarAndMD(samRecord);
-			snpFrequencies.processSequence(samRecord);
-			combinedCigarMDtagList.add(snpFrequencies.getCigarMD().toString());
+			variantCallDetection.processSequence(samRecord);
+			combinedCigarMDtagList.add(variantCallDetection.getCigarMD().toString());
 		}
 		assertEquals("89m", combinedCigarMDtagList.get(0));
 		assertEquals("91m", combinedCigarMDtagList.get(1));
@@ -156,8 +156,8 @@ public class SNPFrequenciesTest {
 		List<String> combinedCigarMDtagList = new ArrayList<String>();			
 		for (SAMRecord samRecord : samRecords) {
 	        //printCigarAndMD(samRecord);	
-			snpFrequencies.processSequence(samRecord);
-			combinedCigarMDtagList.add(snpFrequencies.getCigarMD().toString());	
+			variantCallDetection.processSequence(samRecord);
+			combinedCigarMDtagList.add(variantCallDetection.getCigarMD().toString());	
 		}
 		assertEquals("14m1uCA1m1uCG8m1uTA16m1dT20m2uCTCT27m", combinedCigarMDtagList.get(0));
 		assertEquals("6m1dT34m1uTC13m1uCA36m", combinedCigarMDtagList.get(1));  // reversed and complemented
@@ -171,8 +171,8 @@ public class SNPFrequenciesTest {
 		List<String> combinedCigarMDtagList = new ArrayList<String>();		
 		for (SAMRecord samRecord : samRecords) {
 			//printCigarAndMD(samRecord);
-			snpFrequencies.processSequence(samRecord);
-			combinedCigarMDtagList.add(snpFrequencies.getCigarMD().toString());			
+			variantCallDetection.processSequence(samRecord);
+			combinedCigarMDtagList.add(variantCallDetection.getCigarMD().toString());			
 		}
 		assertEquals("65m3iCGA22m", combinedCigarMDtagList.get(0));  // reversed and complemented
 		assertEquals("57m1iT31m", combinedCigarMDtagList.get(1));
@@ -186,8 +186,8 @@ public class SNPFrequenciesTest {
 		List<String> combinedCigarMDtagList = new ArrayList<String>();			
 		for (SAMRecord samRecord : samRecords) {
           //printCigarAndMD(samRecord);		
-		  snpFrequencies.processSequence(samRecord);
-		  combinedCigarMDtagList.add(snpFrequencies.getCigarMD().toString());		  
+		  variantCallDetection.processSequence(samRecord);
+		  combinedCigarMDtagList.add(variantCallDetection.getCigarMD().toString());		  
 		}
 		assertEquals("6m1iT2m1dT82m", combinedCigarMDtagList.get(0));
 		assertEquals("21m1uCT10m2dTG56m1dA2m", combinedCigarMDtagList.get(1)); // reversed and complemented
@@ -202,8 +202,8 @@ public class SNPFrequenciesTest {
 		List<String> combinedCigarMDtagList = new ArrayList<String>();			
 		for (SAMRecord samRecord : samRecords) {
           //printCigarAndMD(samRecord);		
-		  snpFrequencies.processSequence(samRecord);
-		  combinedCigarMDtagList.add(snpFrequencies.getCigarMD().toString());		  
+		  variantCallDetection.processSequence(samRecord);
+		  combinedCigarMDtagList.add(variantCallDetection.getCigarMD().toString());		  
 		}
 		assertEquals("89m", combinedCigarMDtagList.get(0));
 		assertEquals("91m", combinedCigarMDtagList.get(1));
@@ -230,13 +230,13 @@ public class SNPFrequenciesTest {
 		if(!loadSAMFile(filename)) { return; }
 		List<String> combinedCigarMDtagList = new ArrayList<String>();
 		for (SAMRecord samRecord : samRecords) {
-		  snpFrequencies.processSequence(samRecord);
+		  variantCallDetection.processSequence(samRecord);
 //		  System.out.println("Name: " + samRecord.getReadName());
 //		  System.out.println("String: " + samRecord.getReadString());
 //		  System.out.println("Flags: " + samRecord.getFlags());		  
-//		  System.out.println("CigarMD: " + snpFrequencies.getCigarMD().toString());	  		  
+//		  System.out.println("CigarMD: " + variantCallDetection.getCigarMD().toString());	  		  
 //        printCigarAndMD(samRecord);
-          combinedCigarMDtagList.add(snpFrequencies.getCigarMD().toString());		  
+          combinedCigarMDtagList.add(variantCallDetection.getCigarMD().toString());		  
 		}
 		assertEquals("86m", combinedCigarMDtagList.get(0));
 		assertEquals("68m1uTA17m", combinedCigarMDtagList.get(1));
@@ -277,8 +277,8 @@ public class SNPFrequenciesTest {
 			try {
 				SAMRecord samRecord = it.next();
 				//printCigarAndMD(samRecord);				
-				snpFrequencies.processSequence(samRecord);
-				//System.out.println("SNPFrequenciesTest.java: CigarMD: " + snpFrequencies.getCigarMD().toString());
+				variantCallDetection.processSequence(samRecord);
+				//System.out.println("VariantCallDetectionTest.java: CigarMD: " + variantCallDetection.getCigarMD().toString());
 			} catch (SAMFormatException sfe) { 
 				System.out.println("SAMFormatException");
 			}
@@ -290,43 +290,43 @@ public class SNPFrequenciesTest {
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-		System.out.println("A->C: " + snpFrequencies.getA2C());
-		System.out.println("A->G: " + snpFrequencies.getA2G());
-		System.out.println("A->T: " + snpFrequencies.getA2T());
-		System.out.println("C->A: " + snpFrequencies.getC2A());
-		System.out.println("C->G: " + snpFrequencies.getC2G());
-		System.out.println("C->T: " + snpFrequencies.getC2T());
-		System.out.println("G->A: " + snpFrequencies.getG2A());
-		System.out.println("G->C: " + snpFrequencies.getG2C());
-		System.out.println("G->T: " + snpFrequencies.getG2T());
-		System.out.println("T->A: " + snpFrequencies.getT2A());
-		System.out.println("T->C: " + snpFrequencies.getT2C());
-		System.out.println("T->G: " + snpFrequencies.getT2G());
-		System.out.println("Tot. Mut.: " + snpFrequencies.getTotalMutations());
-		System.out.println("A Ins: " + snpFrequencies.getAInsertions());
-		System.out.println("C Ins: " + snpFrequencies.getCInsertions());
-		System.out.println("G Ins: " + snpFrequencies.getGInsertions());
-		System.out.println("T Ins: " + snpFrequencies.getTInsertions());
-		System.out.println("N Ins: " + snpFrequencies.getNInsertions());
-		System.out.println("Tot. Ins.: " + snpFrequencies.getTotalInsertions());
-		System.out.println("A Del: " + snpFrequencies.getADeletions());
-		System.out.println("C Del: " + snpFrequencies.getCDeletions());
-		System.out.println("G Del: " + snpFrequencies.getGDeletions());
-		System.out.println("T Del: " + snpFrequencies.getTDeletions());
-		System.out.println("N Del: " + snpFrequencies.getNDeletions());		
-		System.out.println("Tot. Del.: " + snpFrequencies.getTotalDeletions());
-		System.out.println("Total: " + snpFrequencies.getTotal());		
-		System.out.println("Tot. Matches: " + snpFrequencies.getTotalMatches());
-		System.out.println("Skipped regions on the reads: " + snpFrequencies.getReadSkippedRegions());
-		System.out.println("Skipped regions on the reference: " + snpFrequencies.getReferenceSkippedRegions());
+		System.out.println("A->C: " + variantCallDetection.getA2C());
+		System.out.println("A->G: " + variantCallDetection.getA2G());
+		System.out.println("A->T: " + variantCallDetection.getA2T());
+		System.out.println("C->A: " + variantCallDetection.getC2A());
+		System.out.println("C->G: " + variantCallDetection.getC2G());
+		System.out.println("C->T: " + variantCallDetection.getC2T());
+		System.out.println("G->A: " + variantCallDetection.getG2A());
+		System.out.println("G->C: " + variantCallDetection.getG2C());
+		System.out.println("G->T: " + variantCallDetection.getG2T());
+		System.out.println("T->A: " + variantCallDetection.getT2A());
+		System.out.println("T->C: " + variantCallDetection.getT2C());
+		System.out.println("T->G: " + variantCallDetection.getT2G());
+		System.out.println("Tot. Mut.: " + variantCallDetection.getTotalMutations());
+		System.out.println("A Ins: " + variantCallDetection.getAInsertions());
+		System.out.println("C Ins: " + variantCallDetection.getCInsertions());
+		System.out.println("G Ins: " + variantCallDetection.getGInsertions());
+		System.out.println("T Ins: " + variantCallDetection.getTInsertions());
+		System.out.println("N Ins: " + variantCallDetection.getNInsertions());
+		System.out.println("Tot. Ins.: " + variantCallDetection.getTotalInsertions());
+		System.out.println("A Del: " + variantCallDetection.getADeletions());
+		System.out.println("C Del: " + variantCallDetection.getCDeletions());
+		System.out.println("G Del: " + variantCallDetection.getGDeletions());
+		System.out.println("T Del: " + variantCallDetection.getTDeletions());
+		System.out.println("N Del: " + variantCallDetection.getNDeletions());		
+		System.out.println("Tot. Del.: " + variantCallDetection.getTotalDeletions());
+		System.out.println("Total: " + variantCallDetection.getTotal());		
+		System.out.println("Tot. Matches: " + variantCallDetection.getTotalMatches());
+		System.out.println("Skipped regions on the reads: " + variantCallDetection.getReadSkippedRegions());
+		System.out.println("Skipped regions on the reference: " + variantCallDetection.getReferenceSkippedRegions());
 
 		System.out.println("SNP/Indels density for each read position:");
-		long[] snpPos = snpFrequencies.getSNPPos();
-		long[] insertionPos = snpFrequencies.getInsertionPos();
-		long[] deletionPos = snpFrequencies.getDeletionPos();
+		long[] snpPos = variantCallDetection.getSNPPos();
+		long[] insertionPos = variantCallDetection.getInsertionPos();
+		long[] deletionPos = variantCallDetection.getDeletionPos();
 		System.out.println("Position\tSNP   \t\tIns   \t\tDel   ");
 		for(int i=0; i<snpPos.length; i++) {
-			// the above arrays have all the same length (see SNPFrequencies.java for details)
+			// the above arrays have all the same length (see VariantCallDetection.java for details)
 			System.out.println(i + "\t\t" + snpPos[i] + "\t\t" + insertionPos[i] + "\t\t" + deletionPos[i]);
 		}
 	}	
