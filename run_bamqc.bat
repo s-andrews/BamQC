6aa2d7be8d988306f929f665a1a3cf6d784804a2
dallepep <piero.dallepezze@gmail.com>
2015-06-25 11:59:23 +0100

    Included remaining jar libraries. This has been tested and works under Windows 7.

--- run_bamqc.bat
+++ run_bamqc.bat
@@ -1,1 +1,1 @@
-java -Xmx250m -classpath .;./sam-1.103.jar;./jbzip2-0.9.jar uk.ac.babraham.BamQC.BamQCApplication
+java -Xmx250m -classpath .;./sam-1.103.jar;./jbzip2-0.9.jar;./hamcrest-core-1.3.jar;./junit-4.11.jar;./log4j-1.2.16.jar uk.ac.babraham.BamQC.BamQCApplication
