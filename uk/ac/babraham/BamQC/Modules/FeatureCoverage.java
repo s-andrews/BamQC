46ba9f9a3ed20987a32ffad6a17e2863ed689291
dallepep <piero.dallepezze@gmail.com>
2015-10-14 15:03:14 +0100

    removed x axis label as this can be overlapped on certain gtf files.

--- uk/ac/babraham/BamQC/Modules/FeatureCoverage.java
+++ uk/ac/babraham/BamQC/Modules/FeatureCoverage.java
@@ -86,7 +86,7 @@ public class FeatureCoverage extends AbstractQCModule {
 
 	@Override
 	public JPanel getResultsPanel() {
-		return new HorizontalBarGraph(featureNames, readCounts, "Read Number", "Feature Type Read Counts");
+		return new HorizontalBarGraph(featureNames, readCounts, "", "Feature Type Read Counts");
 	}
 
 	@Override
