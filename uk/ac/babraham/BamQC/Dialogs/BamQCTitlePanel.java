6de93b7ba22e46f8d3dc930029cb45ab6661a17b
dallepep <piero.dallepezze@gmail.com>
2015-12-03 09:54:36 +0000

    Copyright until 2016

--- uk/ac/babraham/BamQC/Dialogs/BamQCTitlePanel.java
+++ uk/ac/babraham/BamQC/Dialogs/BamQCTitlePanel.java
@@ -89,7 +89,7 @@ public class BamQCTitlePanel extends JPanel {
 		c.add(website,constraints);
 		constraints.gridy++;
 
-		JLabel copyright = new JLabel("<html>\u00a9 Simon Andrews, Pierre Lindenbaum, Brian Howard, <br>Phil Ewels, Bart Ailey, Piero Dalle Pezze 2011-15</html>", JLabel.CENTER);
+		JLabel copyright = new JLabel("<html>\u00a9 Simon Andrews, Pierre Lindenbaum, Brian Howard, <br>Phil Ewels, Bart Ailey, Piero Dalle Pezze 2011-16</html>", JLabel.CENTER);
 		copyright.setFont(new Font("Dialog",Font.PLAIN,14));
 		c.add(copyright,constraints);
 		constraints.gridy++;
