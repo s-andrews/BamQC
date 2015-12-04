724a914586e4c1ac0811a5c3a06198738f79fe0d
dallepep <piero.dallepezze@gmail.com>
2015-12-04 15:15:05 +0000

    genomes are now stored inside the home folder. This makes much easier when a genome is set via command line.

--- uk/ac/babraham/BamQC/Preferences/BamQCPreferences.java
+++ uk/ac/babraham/BamQC/Preferences/BamQCPreferences.java
@@ -79,32 +79,32 @@ public class BamQCPreferences {
 	private BamQCPreferences () {
 
 		
-		// This code uses apache commons.lang3. If you don't want to use it, you can edit this code and use the commented code just below.
-		if(SystemUtils.IS_OS_MAC_OSX) {
-			// let's store these files inside a folder BamQC
-//			preferencesFile= new File(System.getProperty("user.home") + File.separator + "Library" + File.separator + "BamQC" + File.separator + "bamqc_prefs.txt");
-			saveLocation = new File(System.getProperty("user.home") + File.separator + "Library" + File.separator + "BamQC" + File.separator + "BamQC_files");
-			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
-		} else if (SystemUtils.IS_OS_WINDOWS) {
-			// let's store these files inside a folder BamQC
-//			preferencesFile= new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "bamqc_prefs.txt");
-			saveLocation = new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "BamQC_files");
-			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
-		} else if(SystemUtils.IS_OS_UNIX) {
-			// let's store these files as hidden files inside a folder .bamqc
-//			preferencesFile= new File(System.getProperty("user.home") + File.separator + ".bamqc" + File.separator + "bamqc_prefs.txt");
-			saveLocation = new File(System.getProperty("user.home") + File.separator + ".bamqc" + File.separator + "BamQC_files");
-			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
-		} else {
-			// let's store these files explicitly inside a folder bamqc
-//			preferencesFile= new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "bamqc_prefs.txt");
-			saveLocation = new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "BamQC_files");
-			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
-		}
+//		// This code uses apache commons.lang3. If you don't want to use it, you can edit this code and use the commented code just below.
+//		if(SystemUtils.IS_OS_MAC_OSX) {
+//			// let's store these files inside a folder BamQC
+////			preferencesFile= new File(System.getProperty("user.home") + File.separator + "Library" + File.separator + "BamQC" + File.separator + "bamqc_prefs.txt");
+//			saveLocation = new File(System.getProperty("user.home") + File.separator + "Library" + File.separator + "BamQC" + File.separator + "BamQC_files");
+//			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
+//		} else if (SystemUtils.IS_OS_WINDOWS) {
+//			// let's store these files inside a folder BamQC
+////			preferencesFile= new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "bamqc_prefs.txt");
+//			saveLocation = new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "BamQC_files");
+//			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
+//		} else if(SystemUtils.IS_OS_UNIX) {
+//			// let's store these files as hidden files inside a folder .bamqc
+////			preferencesFile= new File(System.getProperty("user.home") + File.separator + ".bamqc" + File.separator + "bamqc_prefs.txt");
+//			saveLocation = new File(System.getProperty("user.home") + File.separator + ".bamqc" + File.separator + "BamQC_files");
+//			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
+//		} else {
+//			// let's store these files explicitly inside a folder bamqc
+////			preferencesFile= new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "bamqc_prefs.txt");
+//			saveLocation = new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "BamQC_files");
+//			genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
+//		}
 		
 		// Code independent of apache commons.lang3
-		//saveLocation = new File(System.getProperty("user.home") + File.separator + "bamqc" + File.separator + "BamQC_files");
-		//genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
+		saveLocation = new File(System.getProperty("user.home") + File.separator + File.separator + "BamQC_files");
+		genomeBase = new File(saveLocation.getAbsolutePath() + File.separator + "genomes");
 	
 		// This is left outside for now. 
 		preferencesFile= new File(System.getProperty("user.home") + File.separator + "bamqc_prefs.txt");
